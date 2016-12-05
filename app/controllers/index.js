5315d5a1d3a092050c32accf9db3f7d12c8647ed
Karl Patrick Espiritu <wiwa.espiritu@gmail.com>
2016-12-05 15:17:20 +0800

    mongoose connect

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 const api = require('./api')
 
+/**
+ * For Restful requests
+ */
 router.use('/api', api)
 
 module.exports = router
+
--- server.js
+++ server.js
@@ -1,16 +1,32 @@
+'use strict'
+
 require('dotenv').config()
 
-// const config = require('./config')
-// const mongoose = require('mongoose')
-
+const config = require('./config')
+const mongoose = require('mongoose')
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3000
 const controllers = require('./app/controllers')
 
-app.use(controllers)
 app.use(express.static(`${__dirname}/public`))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(controllers)
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+connect()
+  .on('error', console.log)
+  .on('disconnected', connect)
+  .once('open', listen)
+
+function listen () {
+  if (app.get('env') === 'test') return
+  app.listen(port)
+  console.log('Express app started on port ' + port)
+}
+
+function connect () {
+  const options = { server: { socketOptions: { keepAlive: 1 } } }
+  return mongoose.connect(config.db, options).connection
+}
